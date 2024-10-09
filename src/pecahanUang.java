42a00ed80afec084b5eedca277c3589d5bd4357f
Noob mc <noobmc56@gmmail.com>
2024-10-09 18:49:17 +0700

    first commit

---
+++ src/pecahanUang.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class pecahanUang {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+
+
+        input.close();
+    }
+}
